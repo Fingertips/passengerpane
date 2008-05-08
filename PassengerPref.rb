269d5d6f16239d33e5b55d815bf5340ab345d2e5
Eloy Duran <eloy.de.enige@gmail.com>
2008-05-09 01:06:35 +0200

    Added PassengerApplication class, applications array controller and hooked up the bindings to the tableview and buttons.

--- PassengerPref.rb
+++ PassengerPref.rb
@@ -10,12 +10,42 @@ require 'osx/cocoa'
 
 include OSX
 
+class PassengerApplication < NSObject
+  kvc_accessor :host, :path
+  
+  def init
+    if super_init
+      @host, @path = 'foo.local', '/some/path/to/app'
+      self
+    end
+  end
+  
+  def initWithHost_path(host, path)
+    if init
+      @host, @path = host, path
+      self
+    end
+  end
+  
+  def restart(sender)
+    p "restart #{self}"
+  end
+  
+  def remove!
+    p "remove #{self}"
+  end
+end
+
 class PrefPanePassenger < NSPreferencePane
+  ib_outlet :applicationsController
   kvc_accessor :applications
   
   def mainViewDidLoad
-    @applications = [{ 'host' => 'example.local', 'path' => '/some/path/to/app' }]
-    p @applications
+    @applications = [].to_ns
+    
+    #app = PassengerApplication.alloc.initWithHost_path('foo.local', '/some/path/to/app')
+    app = PassengerApplication.alloc.init
+    @applicationsController.addObject app
   end
   
   def add(sender)
@@ -24,6 +54,10 @@ class PrefPanePassenger < NSPreferencePane
   
   def remove(sender)
     p "remove"
+    apps = @applicationsController.selectedObjects
+    p apps
+    apps.each { |app| app.remove! }
+    @applicationsController.removeObjects apps
   end
   
   def restart(sender)
