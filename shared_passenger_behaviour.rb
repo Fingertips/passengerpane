18dc1433217bd90d67cdd1dfb674e9e89198d597
Eloy Duran <eloy.de.enige@gmail.com>
2008-06-20 16:59:47 +0200

    At least show an alert that something went wrong if execution fails.

--- shared_passenger_behaviour.rb
+++ shared_passenger_behaviour.rb
@@ -4,7 +4,20 @@ module SharedPassengerBehaviour
   PASSENGER_APPS_DIR = "/private/etc/apache2/passenger_pane_vhosts"
   
   def execute(command, *args)
-    OSX::SecurityHelper.sharedInstance.executeCommand_withArgs(command, args)
+    if OSX::SecurityHelper.sharedInstance.executeCommand_withArgs(command, args) == 0
+      alert = OSX::NSAlert.alloc.init
+      alert.messageText = 'Something went wrong while trying to save your changes'
+      alert.informativeText = "Please check your console.log for further info and file a bug report  if it is a bug at:\nhttp://fingertips.lighthouseapp.com/projects/13022"
+      alert.objc_send(
+        :beginSheetModalForWindow, PrefPanePassenger.sharedInstance.mainView.window,
+        :modalDelegate, nil,
+        :didEndSelector, nil,
+        :contextInfo, nil
+      )
+      false
+    else
+      true
+    end
   end
   module_function :execute
   
